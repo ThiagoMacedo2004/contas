0f72872bb2a6e56332b232bd42a758d71b218159
ThiagoMacedo2004 <tsouzamacedo@gmail.com>
2025-05-18 20:34:05 -0300

    Ajuste na tela de cadastro de fornecedor.

--- src/app/titulos/cadastrar-novo-titulo/cadastrar-novo-titulo.component.ts
+++ src/app/titulos/cadastrar-novo-titulo/cadastrar-novo-titulo.component.ts
@@ -1,13 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { FormBuilder, UntypedFormGroup, Validators } from '@angular/forms';
+import { STEPPER_GLOBAL_OPTIONS } from '@angular/cdk/stepper';
 
 @Component({
   selector: 'app-cadastrar-novo-titulo',
   standalone: true,
   imports: [CommonModule, SharedModule],
   templateUrl: './cadastrar-novo-titulo.component.html',
-  styleUrls: ['./cadastrar-novo-titulo.component.css']
+  styleUrls: ['./cadastrar-novo-titulo.component.css'],
+   providers: [
+    {
+      provide: STEPPER_GLOBAL_OPTIONS,
+      useValue: {showError: true},
+    },
+  ],
 })
 export class CadastrarNovoTituloComponent implements OnInit {
 
@@ -17,11 +25,35 @@ export class CadastrarNovoTituloComponent implements OnInit {
     mask: [/\d/, /\d/, '/', /\d/, /\d/, '/',/\d/, /\d/,/\d/, /\d/]
   };
 
+  formFornecedor!: UntypedFormGroup
+  formDadosPagamento!: UntypedFormGroup
+
   public myModel = ''
 
-  constructor() { }
+  constructor(
+    private _fb: FormBuilder,
+
+  ) { }
 
   ngOnInit(): void {
+    this.formularioFornecedor()
+    this.formularioDadosPagamento()
+  }
+
+  formularioFornecedor() {
+    this.formFornecedor = this._fb.group({
+      EMPRESA         : ['', Validators.required],
+      // FORNECEDOR      : ['', Validators.required],
+      // NF              : ['', Validators.required],
+      // COD_FORNECEDOR  : ['', Validators.required],
+      // ITEM_FORNECEDOR : ['', Validators.required]
+    })
+  }
+
+  formularioDadosPagamento() {
+    this.formDadosPagamento = this._fb.group({
+      CONTA_FLUXO: ['', Validators.required]
+    })
   }
 
 }
