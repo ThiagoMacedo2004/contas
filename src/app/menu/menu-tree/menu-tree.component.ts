e29bf499a8abc6be6420a766a9c74f2a3f4d9c65
ThiagoMacedo2004 <tsouzamacedo@gmail.com>
2025-05-27 15:53:47 -0300

    Ajuste no menu lateral.

--- src/app/menu/menu-tree/menu-tree.component.ts
+++ src/app/menu/menu-tree/menu-tree.component.ts
@@ -1,6 +1,8 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { ActivatedRoute, Router } from '@angular/router';
+import { map, tap } from 'rxjs';
 
 
 @Component({
@@ -12,6 +14,8 @@ import { SharedModule } from 'src/app/shared/shared.module';
 })
 export class MenuTreeComponent implements OnInit {
 
+
+
   links: any[] = [
     {router: 'titulos', titulo: 'Lista de títulos', icon: 'list' },
     {router: 'fornecedores', titulo: 'Fornecedores', icon: 'source_environment' },
@@ -19,6 +23,10 @@ export class MenuTreeComponent implements OnInit {
     {router: 'contas', titulo: 'Contas fluxo', icon: 'wallet' },
     {router: 'contrato', titulo: 'Contratos', icon: 'cases' },
   ]
+
+  constructor(private _rotaAtiva: ActivatedRoute) {
+    console.log(this._rotaAtiva.snapshot.url)
+  }
 
   ngOnInit(): void {
 
